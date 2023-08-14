068ed1c29bab27a9c47bf5ce4dee1fa6f3f45938
cjaem <84785975+jaem-code@users.noreply.github.com>
2023-08-14 13:32:12 +0900

    Revert "stage5"
    This reverts commit 06d7d0eb339ee0cb9c279ce9a2d87e747d3f65bb.

--- Web Quiz Engine (Kotlin)/task/src/engine/config/WebSecurityConfig.kt
+++ Web Quiz Engine (Kotlin)/task/src/engine/config/WebSecurityConfig.kt
@@ -24,9 +24,7 @@ class WebSecurityConfig(
             securityMatcher("/api/quizzes/**")  // "/api/quizzes/**" 패턴에 대한 보안 설정
             csrf { disable() }  // CSRF 보안 비활성화
             authorizeHttpRequests {
-                authorize("/register", permitAll)
-                authorize(anyRequest, authenticated) // 모든 요청에 대해 "USER" 권한이 필요
-                authorize("/actuator/shutdown", permitAll)  // Allow access without authentication
+                authorize(anyRequest, hasRole("USER"))  // 모든 요청에 대해 "USER" 권한이 필요
             }
             httpBasic { }  // HTTP Basic 인증 사용
         }
